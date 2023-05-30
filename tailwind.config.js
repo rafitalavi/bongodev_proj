959b32cf13ffef6117fbf45b53e8fb707a4f16cc
rafitalavi <rafitalavi12345@gmail.com>
2023-05-30 23:05:12 +0600

    Imgvault

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      "top-nav": "#191922",
+      "sub-nav": "#353131",
+      "f-color": "#FFF",
+      "main-body-bg": "#808080",
+      "f-dark-color":"#000000"
+
+    }
+  },
+  
+  plugins: [],
+}
+
